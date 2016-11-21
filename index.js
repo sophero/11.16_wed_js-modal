2c713f2ff9b6276f433545ae6aa16017a66dd2cb
Sophia Di <backwardsxmarathon@gmail.com>
2016-11-20 21:51:12 -0500

    Edited nav bar entries. Added horizontal line between nav bar and content as mainpage-content border-top. Added content to main page and narrowed mainpage-content width.

--- index.js
+++ index.js
@@ -9,18 +9,15 @@ function closeModal() {
 	modal.className = "modal";
 }
 
+// Open the modal
+window.setTimeout(openModal, 3000);
+
+// Closing the modal
+
 closeButton.addEventListener("click", closeModal);
+
 document.addEventListener("keydown", function(event) {
 	if (event.keyCode == 27) {
 		closeModal();
 	}
 });
-
-// signUpButton = document.querySelector(".signup-button");
-// signUpButton.addEventListener("mouseenter", function () {
-// 	signUpButton.innerText = "Sign Up!";
-// });
-
-
-window.setTimeout(openModal, 3000);
-// Change this to three seconds!!!!!!!
